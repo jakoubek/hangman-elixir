53a12c059d23e8db792a80a36838340a93a964f1
Oliver Jakoubek <mail@oliverjakoubek.de>
2022-02-20 16:01:30 +0100

    Added a GenServer module to game logic

---
+++ hangman/lib/runtime/server.ex
@@ -0,0 +1,29 @@
+defmodule Hangman.Runtime.Server do
+
+  use GenServer
+
+  alias Hangman.Impl.Game
+
+  ### client process
+
+  def start_link do
+    GenServer.start_link(__MODULE__, nil)
+  end
+
+
+  ### server process
+
+  def init(_args) do
+    { :ok, Game.new_game }
+  end
+
+  def handle_call({ :make_move, guess }, _from, game) do
+    { updated_game, tally } = Game.make_move(game, guess)
+    { :reply, tally, updated_game }
+  end
+
+  def handle_call({ :tally }, _from, game) do
+    { :reply, Game.tally(game), game }
+  end
+
+end
