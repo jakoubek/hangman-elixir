a365eb0b5d8cdd6188beac9806de1dc8f0d72199
Oliver Jakoubek <mail@oliverjakoubek.de>
2022-02-20 11:45:26 +0100

    Added a supervisor to the dictionary application

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -4,13 +4,18 @@ defmodule Dictionary.Runtime.Server do
 
   @me __MODULE__
 
+  use Agent
+
   alias Dictionary.Impl.WordList
 
-  def start_link do
+  def start_link(_args) do
     Agent.start_link(&WordList.word_list/0, name: @me)
   end
 
   def random_word() do
+    if :rand.uniform < 0.33 do
+      Agent.get(@me, fn _ -> exit(:boom) end)
+    end
     Agent.get(@me, &WordList.random_word/1)
   end
 
